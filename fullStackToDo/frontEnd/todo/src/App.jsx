1ece6397f528de374c232e3b1dbdc572255e7c2f
ibrar hassan <ibrarhassan00@outlook.com>
2025-10-16 15:42:40 +0500

    CRUD_Complete

--- fullStackToDo/frontEnd/todo/src/App.jsx
+++ fullStackToDo/frontEnd/todo/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [todo, setTodo] = useState("")
   const [tododata , getTodoData] =useState([]) 
   
+  useEffect(() => {
+    getTodo();
+  }, []);
+
   let addTodo = async()=>{
     try {
    const body ={
@@ -17,7 +21,7 @@ function App() {
     body:JSON.stringify(body)
    })
    .then(res=>res.json())
-   console.log(response);
+  //  console.log(response);
    setTodo("");
    getTodoData([])
    getTodo()
@@ -29,22 +33,63 @@ function App() {
 
   let getTodo = async()=>{
   const response = await fetch(`http://localhost:5000/gettodo`).then(res=>res.json())
-  console.log(response.data);
+  // console.log(response.data[1]._id);
   getTodoData(response.data)
  
+  }
+
+  let editTodo = async(element)=>{
+try {
+  let editValue = prompt()
+    let todoId = element.target.id;
+    let obj = {
+      todo : editValue,
+    }
+    await fetch(`http://localhost:5000/updatetodo/${todoId}`, {method:"put",
+      headers:{
+       "content-type" : "application/json"
+      },
+      body:JSON.stringify(obj)
+    })
+    getTodo()
+
+} catch (error) {
+alert(error.message)  
+}
+
+
+
+
+  }
+
+  let deleteTodo = async(element)=>{
+  try {
+    let todoId = element.target.id;
+  await fetch(`http://localhost:5000/deletetodo?id=${todoId}`,{method:"DELETE",
+    headers:{"content-type" : "application/json"}
+  })
+  getTodo()
+  } catch (error) {
+    alert(error.message)
+    
+  }
   }
 
   return (
     <>
       <input type="text" placeholder='Enter todo' onChange={(value) => {
         setTodo(value.target.value)
-      }} />
+      }} value={todo} />
       <button onClick={addTodo} >Add</button>
-     <div>
+     <ul>
       {tododata.map((value,index)=>{
-        return <h1 key={index}>{value.todo}</h1>
+        return <li key={index}>
+          <p>{value.todo}</p>
+          <button id={value._id} onClick={editTodo} >Edit</button>
+          <button id={value._id} onClick={deleteTodo} >Delete</button>
+        </li>
       })}
-      </div> 
+      </ul> 
  
     </>
   )
